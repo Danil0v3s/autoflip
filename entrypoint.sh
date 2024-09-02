80e6298993b2d00d1c8750175db4a41919219665
Danilo Lemes <daniloleemes@hotmail.com>
2024-09-02 17:27:09 +0100

    remove tmp low res file

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,3 +8,5 @@ ffmpeg -y -i input/${input_video} -vf "scale=-2:480,fps=24" input/input_low.mp4
 bazel-bin/mediapipe/examples/desktop/autoflip/run_autoflip \
         --calculator_graph_config_file=input/autoflip_graph.pbtxt \
         --input_side_packets=input_video_path=input/input_low.mp4,output_video_path=output/video.mp4,aspect_ratio="${aspect_ratio}"
+
+rm input/input_low.mp4
