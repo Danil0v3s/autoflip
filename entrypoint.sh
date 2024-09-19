0995aaefddfc4e0ef55072a828a96f9d3e0e276e
Danilo Lemes <daniloleemes@hotmail.com>
2024-09-19 09:57:16 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ aspect_ratio=$2
 video_filename=${input_video%.*}
 
 echo "Downscaling video to 480w..."
-ffmpeg -hide_banner -loglevel error -y -i input/${input_video} -vf "scale=-2:480,fps=24" input/input_low.mp4
+#ffmpeg -hide_banner -loglevel error -y -i input/${input_video} -vf "scale=-2:480,fps=24" input/input_low.mp4
 
 cd src && bazel-bin/mediapipe/examples/desktop/autoflip/run_autoflip \
         --calculator_graph_config_file=../input/autoflip_graph.pbtxt \
-        --input_side_packets=input_video_path=../input/input_low.mp4,output_video_path=../output/"${video_filename}"_"${aspect_ratio}".mp4,aspect_ratio="${aspect_ratio}",csv_path=../output/"${video_filename}"_"${aspect_ratio}".csv
+        --input_side_packets=input_video_path=../input/input_low.mp4,output_video_path=../output/"${video_filename}"_"${aspect_ratio}".mp4,aspect_ratio="${aspect_ratio}",csv_path=../output/"${video_filename}"_"${aspect_ratio}".csv,key_frame_crop_viz_frames_path=../output/key_frame.mp4,salient_point_viz_frames_path=../output/salient.mp4
 
 cd ..;
 
-rm input/input_low.mp4
+#rm input/input_low.mp4
